8aa64fca1022b05542a30515fbc4234574263e13
onlyalphy <onlyalphy01@gmail.com>
2024-03-06 21:41:55 +0300

    first commit

---
+++ PLP HOMEWORK/user_input.py
@@ -0,0 +1,8 @@
+# Ask the user for their name
+name = input("What is your name:Alphonce")
+# Ask the user for their age
+age = input("How old are you:21")
+# Ask the user for their location
+location = input("Where do you live:Thika")
+#print out a personalized message
+print ("Hello my name is  {name}, I am {age} years old and live in {location}")
